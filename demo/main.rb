ff4a9a97bd8140573606a97da2195960634c1b5d
Dino Reic <rejotl@gmail.com>
2025-02-01 18:26:18 +0100

    added tabs in examples

--- demo/main.rb
+++ demo/main.rb
@@ -24,23 +24,30 @@ def fez name
   end
 
   out.push %{
-    <div class="flex" style="border-top: 1px solid #ccc; margin-top: 40px;">
-      <div class="body">
+    <div class="flex" style="border-top: 1px solid #ccc; margin-bottom: 50px;">
+      <div style="margin-top: 10px;">
         <h2 style="margin-top: 0;">Demo: ui-#{name}</h2>
-        #{body[0]}
 
         <div id="body-#{name}" style="padding-bottom: 20px;">
           #{body[1]}
         </div>
-
-        <ui-editor name="#{name}" id="html-#{name}" file="#{filePath.sub(/\.\w+$/,'.html')}" language="html" :action="applyChanges">
-           #{escape(body[1])}
-        </ui-editor>
       </div>
-      <div>
-        <ui-editor id="code-ui-#{name}" file="#{filePath}" name="#{name}" :action="applyChanges">
-           #{escape(File.read(filePath))}
-        </ui-editor>
+      <div style="margin-top: 20px;">
+        <ui-tabs>
+          <div title="Info" active="true">
+            #{body[0]}
+          </div>
+          <div title="HTML">
+            <ui-editor name="#{name}" id="html-#{name}" file="#{filePath.sub(/\.\w+$/,'.html')}" language="html" :action="applyChanges">
+              #{escape(body[1])}
+            </ui-editor>
+          </div>
+          <div title="Fez component">
+            <ui-editor id="code-ui-#{name}" file="#{filePath}" name="#{name}" :action="applyChanges">
+              #{escape(File.read(filePath))}
+            </ui-editor>
+          </div>
+        </ui-tabs>
       </div>
     </div>
   }
